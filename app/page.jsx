520b3bc37b531b767c195f52c3d7c6491792150c
Om Jogani <om080jogani@gmail.com>
2023-12-15 17:36:15 +0530

    [Add]: Update the Authentication

---
+++ app/page.jsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Use & Share
+        <br className="max-md:hidden"/>
+        <span className="orange_gradient text-center"> AI-Powered Prompt</span>
+      </h1>
+      <p className="desc text-center">
+        Prompt Engineering is really best way to utilize the power of AI, Use & Share Now!!
+      </p>
+    </section>
+  )
+}
+
+export default Home

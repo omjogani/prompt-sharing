d7d81c3d89a24a91492289f78392369a24dd39da
Om Jogani <om080jogani@gmail.com>
2023-12-30 21:11:22 +0530

    [Update]: Update Prompt API Added

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -15,6 +15,27 @@ export const GET = async (request, { params }) => {
   }
 };
 
+
+// PATCH (update)
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+      await connectToDB();
+      const existingPrompt = await Prompt.findById(params.id);
+      if(!existingPrompt){
+          return new Response("Prompt not Found!", { status: 400 });
+      }
+      existingPrompt.prompt = prompt;
+      existingPrompt.tag = tag;
+
+      await existingPrompt.save();
+      return new Response(JSON.stringify(existingPrompt), { status: 200 });
+  } catch (error) {
+      return new Response("Failed to Update Prompts", { status:500 });
+  }
+}
+
 // DELETE (Delete a prompt)
 export const DELETE = async (request, { params }) => {
     try {
